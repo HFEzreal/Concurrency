315af51c83957ca71124ab3d2fdbc8c98f2127a9
HFEzreal <1272206911@qq.com>
2020-01-14 17:26:45 +0800

    提交代码

--- src/main/java/com/liubin/Concurrency/thread/RejectedExecutionHandlerTest.java
+++ src/main/java/com/liubin/Concurrency/thread/RejectedExecutionHandlerTest.java
@@ -1,8 +1,6 @@
 package com.liubin.Concurrency.thread;
 
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 /**
  * @description 拒绝策略
@@ -16,12 +14,14 @@ public class RejectedExecutionHandlerTest {
 
     public static void main(String[] args) throws Exception {
         //新建一个线程池，corePoolSize和maximumPoolSize都是1，ArrayBlockingQueue是有界队列，容量设置为1
-        ThreadPoolExecutor pool = new ThreadPoolExecutor(THREADS_SIZE, THREADS_SIZE, 0, TimeUnit.SECONDS, new ArrayBlockingQueue<Runnable>(CAPACITY));
+//        ThreadPoolExecutor pool = new ThreadPoolExecutor(THREADS_SIZE, THREADS_SIZE, 0, TimeUnit.SECONDS, new ArrayBlockingQueue<Runnable>(CAPACITY));
+//        ThreadPoolExecutor pool = new ThreadPoolExecutor(THREADS_SIZE, THREADS_SIZE, 0, TimeUnit.SECONDS, new LinkedBlockingQueue<>());
+        ThreadPoolExecutor pool = new ThreadPoolExecutor(THREADS_SIZE, THREADS_SIZE, 0, TimeUnit.SECONDS, new SynchronousQueue<>());
         //设置拒绝策略
-        pool.setRejectedExecutionHandler(new ThreadPoolExecutor.AbortPolicy());//直接丢弃当前任务，并抛出RejectedExecutionException异常，默认
-        pool.setRejectedExecutionHandler(new ThreadPoolExecutor.DiscardPolicy());//直接丢弃当前任务
-        pool.setRejectedExecutionHandler(new ThreadPoolExecutor.DiscardOldestPolicy());//丢弃队列里面最前面的任务，然后重新尝试执行任务（重复此过程）
-        pool.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());//由调用线程处理该任务
+//        pool.setRejectedExecutionHandler(new ThreadPoolExecutor.AbortPolicy());//直接丢弃当前任务，并抛出RejectedExecutionException异常，默认
+//        pool.setRejectedExecutionHandler(new ThreadPoolExecutor.DiscardPolicy());//直接丢弃当前任务
+//        pool.setRejectedExecutionHandler(new ThreadPoolExecutor.DiscardOldestPolicy());//丢弃队列里面最前面的任务，然后重新尝试执行任务（重复此过程）
+//        pool.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());//由调用线程处理该任务
 
         //新建10个任务，把他们放到线程中
         for(int i=0;i<10;i++){
