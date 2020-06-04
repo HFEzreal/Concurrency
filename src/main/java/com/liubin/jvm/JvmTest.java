58506f8bb8c720a7050b21952837712597a5b77e
liubin <liubin7823@163.com>
2020-06-04 23:23:52 +0800

    添加垃圾回收

---
+++ src/main/java/com/liubin/jvm/JvmTest.java
@@ -0,0 +1,15 @@
+package com.liubin.jvm;
+
+/**
+ * @author liubin
+ * @description
+ * @date 20/6/4 21:50
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        byte[] allocation1, allocation2;
+        allocation1 = new byte[30900 * 1024];
+        //allocation2 = new byte[900*1024];
+    }
+}
