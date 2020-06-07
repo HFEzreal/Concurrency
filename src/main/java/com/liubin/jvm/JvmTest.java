f7efc3266c9d7f1bdc025ce6afd37076fa0994d4
liubin <liubin7823@163.com>
2020-06-07 23:07:39 +0800

    123

--- src/main/java/com/liubin/jvm/JvmTest.java
+++ src/main/java/com/liubin/jvm/JvmTest.java
@@ -5,11 +5,18 @@ package com.liubin.jvm;
  * @description
  * @date 20/6/4 21:50
  */
-public class Test {
+public class JvmTest {
 
     public static void main(String[] args) {
-        byte[] allocation1, allocation2;
-        allocation1 = new byte[30900 * 1024];
-        //allocation2 = new byte[900*1024];
+//        byte[] allocation1, allocation2;
+//        allocation1 = new byte[30900 * 1024];
+//        allocation2 = new byte[900*1024];
+
+        byte[] allocation1, allocation2,allocation3,allocation4,allocation5;
+        allocation1 = new byte[32000*1024];
+        allocation2 = new byte[1000*1024];
+        allocation3 = new byte[1000*1024];
+        allocation4 = new byte[1000*1024];
+        allocation5 = new byte[1000*1024];
     }
 }
