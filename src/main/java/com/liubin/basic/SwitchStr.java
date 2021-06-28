7d4fb014036009eb48f970a89202d15ccf773f0f
HFEzreal <1272206911@qq.com>
2021-06-28 20:17:38 +0800

    添加对象加载

---
+++ src/main/java/com/liubin/basic/SwitchStr.java
@@ -0,0 +1,19 @@
+package com.liubin.basic;
+
+
+public class SwitchStr {
+
+    public static void main(String[] args) {
+        String str1 = "1";
+        switch (str1) {
+            case "1":
+                System.out.println("1");
+                break;
+            case "2":
+                System.out.println("2");
+                break;
+            default:
+                System.out.println("3");
+        }
+    }
+}
---
+++ src/main/java/com/liubin/basic/Test.java
@@ -0,0 +1,12 @@
+package com.liubin.basic;
+
+public class Test {
+    // 类变量
+    private static int i1 = 1;
+    // 成员变量
+    private int i2;
+    // 局部变量
+    private void test(int i3){
+        int i4;
+    }
+}
